02b2c5f6ffe0266cb9ca833edb17fb191bf37664
Michael Ehrenreich <michael.ehrenreich@ottogroup.com>
2023-10-18 09:14:31 +0200

    fix(): Prepend the CWD to the path

--- merge-kubeconfigs.sh
+++ merge-kubeconfigs.sh
@@ -5,10 +5,16 @@ KUBECONFIG=""
 
 for file in $CONFIG_FILES
 do
+    if [[ ! $(echo $file | grep ^/) ]]
+    then
+        file=$PWD/$file
+    fi
     if [[ -f $file ]]
     then
         KUBECONFIG="${KUBECONFIG}:${file}"
     fi
 done
+
+export KUBECONFIG=${KUBECONFIG/:/}
 
 kubectl config view --flatten
